c98def306654c03daaa41524b140a93d768017a8
Lukas Jendele <lukas.jendele@gmail.com>
2016-07-03 19:07:09 +0200

    Prvni verze

--- MamaTrainingTimer/Form1.Designer.cs
+++ MamaTrainingTimer/Form1.Designer.cs
@@ -29,11 +29,243 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numPhases = new System.Windows.Forms.NumericUpDown();
+            this.breakDurationPicker = new System.Windows.Forms.DateTimePicker();
+            this.phaseDurationPicker = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.SettingsTab = new System.Windows.Forms.TabPage();
+            this.ProgressTab = new System.Windows.Forms.TabPage();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblAction = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPhases)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SettingsTab.SuspendLayout();
+            this.ProgressTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(46, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of phases:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(44, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phase duration:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(44, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Break duration:";
+            // 
+            // numPhases
+            // 
+            this.numPhases.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numPhases.Location = new System.Drawing.Point(361, 96);
+            this.numPhases.MinimumSize = new System.Drawing.Size(106, 0);
+            this.numPhases.Name = "numPhases";
+            this.numPhases.Size = new System.Drawing.Size(106, 22);
+            this.numPhases.TabIndex = 3;
+            this.numPhases.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // breakDurationPicker
+            // 
+            this.breakDurationPicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.breakDurationPicker.Location = new System.Drawing.Point(359, 162);
+            this.breakDurationPicker.Name = "breakDurationPicker";
+            this.breakDurationPicker.Size = new System.Drawing.Size(106, 22);
+            this.breakDurationPicker.TabIndex = 5;
+            this.breakDurationPicker.Value = new System.DateTime(2016, 7, 3, 9, 27, 0, 0);
+            // 
+            // phaseDurationPicker
+            // 
+            this.phaseDurationPicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.phaseDurationPicker.Location = new System.Drawing.Point(361, 126);
+            this.phaseDurationPicker.Name = "phaseDurationPicker";
+            this.phaseDurationPicker.Size = new System.Drawing.Size(106, 22);
+            this.phaseDurationPicker.TabIndex = 6;
+            this.phaseDurationPicker.Value = new System.DateTime(2016, 7, 3, 9, 27, 0, 0);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Location = new System.Drawing.Point(187, 219);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(127, 40);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.SettingsTab);
+            this.tabControl1.Controls.Add(this.ProgressTab);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(513, 397);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // SettingsTab
+            // 
+            this.SettingsTab.Controls.Add(this.label1);
+            this.SettingsTab.Controls.Add(this.btnStart);
+            this.SettingsTab.Controls.Add(this.label2);
+            this.SettingsTab.Controls.Add(this.phaseDurationPicker);
+            this.SettingsTab.Controls.Add(this.label3);
+            this.SettingsTab.Controls.Add(this.breakDurationPicker);
+            this.SettingsTab.Controls.Add(this.numPhases);
+            this.SettingsTab.Location = new System.Drawing.Point(4, 25);
+            this.SettingsTab.Name = "SettingsTab";
+            this.SettingsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.SettingsTab.Size = new System.Drawing.Size(505, 368);
+            this.SettingsTab.TabIndex = 0;
+            this.SettingsTab.Text = "Settings";
+            this.SettingsTab.UseVisualStyleBackColor = true;
+            // 
+            // ProgressTab
+            // 
+            this.ProgressTab.Controls.Add(this.lblTime);
+            this.ProgressTab.Controls.Add(this.btnStop);
+            this.ProgressTab.Controls.Add(this.lblAction);
+            this.ProgressTab.Controls.Add(this.pictureBox1);
+            this.ProgressTab.Location = new System.Drawing.Point(4, 25);
+            this.ProgressTab.Name = "ProgressTab";
+            this.ProgressTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ProgressTab.Size = new System.Drawing.Size(505, 368);
+            this.ProgressTab.TabIndex = 1;
+            this.ProgressTab.Text = "Progress";
+            this.ProgressTab.UseVisualStyleBackColor = true;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTime.Location = new System.Drawing.Point(0, 310);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(326, 53);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "Time elapsed:";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStop.Location = new System.Drawing.Point(366, 310);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(136, 55);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblAction
+            // 
+            this.lblAction.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAction.Location = new System.Drawing.Point(3, 3);
+            this.lblAction.Name = "lblAction";
+            this.lblAction.Size = new System.Drawing.Size(499, 32);
+            this.lblAction.TabIndex = 1;
+            this.lblAction.Text = "Action";
+            this.lblAction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(94, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 255);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(513, 397);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(466, 409);
+            this.Name = "Form1";
+            this.Text = "Training timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numPhases)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.SettingsTab.ResumeLayout(false);
+            this.SettingsTab.PerformLayout();
+            this.ProgressTab.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numPhases;
+        private System.Windows.Forms.DateTimePicker breakDurationPicker;
+        private System.Windows.Forms.DateTimePicker phaseDurationPicker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage SettingsTab;
+        private System.Windows.Forms.TabPage ProgressTab;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblAction;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTime;
     }
 }
 
--- MamaTrainingTimer/Form1.cs
+++ MamaTrainingTimer/Form1.cs
@@ -1,20 +1,235 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Reflection;
 
 namespace MamaTrainingTimer
 {
     public partial class Form1 : Form
     {
+        private bool speakingEnabled;
+
+        private DateTime totalStart;
+        private TimeSpan phaseDuration;
+        private TimeSpan breakDuration;
+        int numOfPhasesProcessed = 0;
+        int numOfBreaksProcessed = 0;
+        int numOfPhases = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+            var args = Environment.GetCommandLineArgs();
+            if (args.Contains("-notalk"))
+                speakingEnabled = false;
+            else
+                speakingEnabled = true;
+
+            // UI Setup
+            breakDurationPicker.Format = DateTimePickerFormat.Time;
+            breakDurationPicker.ShowUpDown = true;
+            breakDurationPicker.Value = new DateTime(2016, 3, 19, 0, 0, 20);
+            phaseDurationPicker.Value = new DateTime(2016, 3, 19, 0, 0, 30);
+            phaseDurationPicker.Format = DateTimePickerFormat.Time;
+            phaseDurationPicker.ShowUpDown = true;
+
+            tabControl1.TabPages.Remove(ProgressTab);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        public Prompt Speak(string textToSpeak, bool sync = false)
+        {
+
+            if (speakingEnabled)
+            {
+                if (sync)
+                {
+                    Speaker.Instance?.Speak(textToSpeak);
+                }
+                else
+                    return Speaker.Instance?.SpeakAsync(textToSpeak);
+            }
+            return null;
+        }
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Add(ProgressTab);
+            tabControl1.TabPages.Remove(SettingsTab);
+
+            totalStart = DateTime.Now;
+            phaseDuration = phaseDurationPicker.Value.TimeOfDay;
+            breakDuration = breakDurationPicker.Value.TimeOfDay;
+            numOfPhases = (int)numPhases.Value;
+            numOfBreaksProcessed = numOfPhasesProcessed = 0;
+            lblAction.Text = "Get ready!";
+            lblTime.Text = "";
+
+            var img = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            Graphics g = Graphics.FromImage(img);
+            g.Clear(Color.White);
+            pictureBox1.Image = img;
+
+            Speak("3 2 1, Go!", true);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var elapsed = DateTime.Now - totalStart;
+            var totalDuration = totalStart.Add(new TimeSpan(phaseDuration.Ticks * (numOfPhases) + breakDuration.Ticks * (numOfPhases - 1))) - totalStart;
+            if (elapsed >= totalDuration)
+            {
+                Speak("You completed your training!");
+                lblAction.Text = "You completed your training!";
+                PlaySound("Completed.wav");
+                timer1.Stop();
+
+                var img = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+                Graphics g = Graphics.FromImage(img);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+                g.FillEllipse(Brushes.Green, pictureBox1.ClientRectangle);
+                pictureBox1.Image = img;
+
+                return;
+            }
+
+            // determine the phase
+            int numOfPhasesAndBreaks = (numOfPhases * 2) - 1;
+            bool isCurrentPhase = false;
+            int currentPhaseBreakIndex = 0;
+            TimeSpan t = new TimeSpan(0);
+            int counter = 0;
+            while (t <= totalDuration)
+            {
+                if (t >= elapsed)
+                {
+                    isCurrentPhase = (counter - 1) % 2 == 0;
+                    currentPhaseBreakIndex = (counter - 1) / 2;
+                    lblTime.Text = $"Remaining to next phase/break: {(t - elapsed).TotalSeconds.ToString("f1")} secs";
+                    break;
+                }
+
+                t = ++counter % 2 == 0 ? t = t.Add(breakDuration) : t = t.Add(phaseDuration);
+            }
+
+            float phaseBreakPercent;
+            if (isCurrentPhase)
+            {
+                lblAction.Text = $"Phase: {currentPhaseBreakIndex + 1}";
+                if (currentPhaseBreakIndex >= numOfPhasesProcessed)
+                {
+                    numOfPhasesProcessed++;
+                    if (currentPhaseBreakIndex + 1 != 1)
+                        PlaySound("Next.wav");
+                    Speak(lblAction.Text);
+                }
+                phaseBreakPercent = 1 - ((float)(t - elapsed).Ticks / phaseDuration.Ticks);
+            }
+            else
+            {
+                lblAction.Text = $"Break: {currentPhaseBreakIndex + 1}";
+                if (currentPhaseBreakIndex >= numOfBreaksProcessed)
+                {
+                    numOfBreaksProcessed++;
+                    PlaySound("Next.wav");
+                    Speak("Take a break!");
+                }
+                phaseBreakPercent = 1 - ((float)(t - elapsed).Ticks / breakDuration.Ticks);
+            }
+
+
+            DrawPicture(pictureBox1.Image, (float)elapsed.Ticks / totalDuration.Ticks, phaseBreakPercent);
+        }
+
+        private void PlaySound(string name)
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("MamaTrainingTimer." + name);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(stream);
+            player.Play();
+        }
+
+        private void DrawPicture(Image img, float totalPercent, float phasePercent)
+        {
+            var now = DateTime.Now;
+            img?.Dispose();
+            img = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            Graphics g = Graphics.FromImage(img);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.Clear(Color.White);
+
+            g.FillEllipse(Brushes.Red, pictureBox1.ClientRectangle);
+            g.FillPie(Brushes.Green, pictureBox1.ClientRectangle, -90, (float)(totalPercent * 360));
+
+            Rectangle smallerRectangle = pictureBox1.ClientRectangle;
+            smallerRectangle.Inflate((int)(-0.1 * pictureBox1.ClientRectangle.Width), (int)(-0.1 * pictureBox1.ClientRectangle.Height));
+            g.FillEllipse(Brushes.Blue, smallerRectangle);
+            g.FillPie(Brushes.Orange, smallerRectangle, -90, (float)(phasePercent * 360));
+
+            pictureBox1.Image = img;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            tabControl1.TabPages.Add(SettingsTab);
+            tabControl1.TabPages.Remove(ProgressTab);
         }
     }
+
+    public class Speaker
+    {
+        private static SpeechSynthesizer synthesizer;
+        private static bool possible = true;
+        public static SpeechSynthesizer Instance
+        {
+            get
+            {
+                if (possible && synthesizer == null)
+                {
+                    synthesizer = new SpeechSynthesizer();
+                    if (!IsEnglish(synthesizer.Voice))
+                    {
+                        foreach (var voice in synthesizer.GetInstalledVoices())
+                        {
+                            if (IsEnglish(voice.VoiceInfo))
+                            {
+                                synthesizer.SelectVoice(voice.VoiceInfo.Name);
+                                possible = true;
+                                break;
+                            }
+                            possible = false;
+                        }
+                        if (!possible) synthesizer = null;
+                    }
+                }
+                return synthesizer;
+            }
+        }
+
+        private static bool IsEnglish(VoiceInfo voice)
+        {
+            return voice.Culture.EnglishName.ToLower().StartsWith("english");
+        }
+    }
+
 }
+
+
